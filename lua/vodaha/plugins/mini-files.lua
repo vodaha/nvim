bced54c0fa783399577e0f235e7f1a3aafefe786
Said Ahadov 17939 <said@saids-MacBook-Pro-2.local>
2024-09-06 13:43:09 +0400

    mini file explorer added

---
+++ lua/vodaha/plugins/mini-files.lua
@@ -0,0 +1,18 @@
+return {
+    'echasnovski/mini.nvim', version = false,
+
+    config = function()
+        local mini_files = require('mini.files')
+
+        local set = vim.keymap.set
+
+        set("n", "<leader>xd", mini_files.open)
+
+        mini_files.setup{
+            mappings = {
+                go_in      = 'L',
+                go_in_plus = 'l'
+            }
+        }
+    end
+}
